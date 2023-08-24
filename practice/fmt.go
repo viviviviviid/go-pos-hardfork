a9a6daaf21bfb85df29ca39c33041e1e44945dc2
taptap-kor <tyy8282@gmail.com>
2023-08-24 17:56:03 +0900

    feat: pointers

--- practice/fmt.go
+++ practice/fmt.go
@@ -1,12 +1,12 @@
-package main
+// package main
 
-import "fmt"
+// import "fmt"
 
-func main() {
-	name := 3124124
-	fmt.Println("%b", name) // 내용 그대로 터미널에 출력
-	fmt.Printf("%b", name)  // 변환된 내용을 터미널에 출력
+// func main() {
+// 	name := 3124124
+// 	fmt.Println("%b", name) // 내용 그대로 터미널에 출력
+// 	fmt.Printf("%b", name)  // 변환된 내용을 터미널에 출력
 
-	hi := fmt.Sprintf("%b", name) // 변환된 내용을
-	fmt.Println(name, hi)
-}
+// 	hi := fmt.Sprintf("%b", name) // 변환된 내용을
+// 	fmt.Println(name, hi)
+// }
