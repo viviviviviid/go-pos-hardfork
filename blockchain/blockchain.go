39db6ede52a809348685b3ccf50ad1e73cc50bd7
taptap-kor <tyy8282@gmail.com>
2023-08-24 23:10:46 +0900

    feat: getLastHash function

--- blockchain/blockchain.go
+++ blockchain/blockchain.go
@@ -1,5 +1,11 @@
 package blockchain
 
+import (
+	"crypto/sha256"
+	"fmt"
+	"sync"
+)
+
 type block struct {
 	data     string
 	hash     string
@@ -7,14 +13,38 @@ type block struct {
 }
 
 type blockchain struct {
-	blocks []block
+	blocks []*block
 }
 
 var b *blockchain
+var once sync.Once // sync 패키지
+
+func (b *block) calculateHash() {
+	hash := sha256.Sum256([]byte(b.data + b.prevHash))
+	b.hash = fmt.Sprintf("%x", hash)
+}
+
+func getLastHash() string {
+	totalBlocks := len(GetBlockchain().blocks)
+	if totalBlocks == 0 {
+		return ""
+	}
+	return GetBlockchain().blocks[totalBlocks-1].hash // 마지막 블록 해시 반환
+}
+
+func createBlock(data string) *block {
+	newBlock := block{data, "", getLastHash()}
+	newBlock.calculateHash()
+	return &newBlock
+}
 
 func GetBlockchain() *blockchain {
 	if b == nil {
-		b = &blockchain{}
+		once.Do(func() { // 한 번만 실행함
+			b = &blockchain{}
+			b.blocks = append(b.blocks, createBlock("Genesis Block"))
+		})
+
 	}
 	return b
 }
