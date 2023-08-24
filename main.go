3a5fbe9b56a3106f532fb4ea58ec7ee398830f68
taptap-kor <tyy8282@gmail.com>
2023-08-24 23:23:56 +0900

    refactor

--- main.go
+++ main.go
@@ -11,6 +11,7 @@ func main() {
 	chain.AddBlock("Second Block")
 	chain.AddBlock("Third Block")
 	chain.AddBlock("Fourth Block")
+
 	for _, block := range chain.AllBlocks() {
 		fmt.Printf("Data: %s\n", block.Data)
 		fmt.Printf("Hash: %s\n", block.Hash)
