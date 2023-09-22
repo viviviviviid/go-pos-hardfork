66b6ee45326dc35465b15f3e50f2f2f0fc2d2d6f
taptap-kor <tyy8282@gmail.com>
2023-09-23 01:12:55 +0900

    bulding: wallet restoring

--- wallet/wallet.go
+++ wallet/wallet.go
@@ -1,11 +1,29 @@
 package wallet
 
-const (
-	hashedMessage string = "1c5863cd55b5a4413fd59f054af57ba3c75c0698b3851d70f99b8de2d5c7338f"
-	privateKey    string = "3077020101042028f3ae83f5b6726ae5587ddc2238665b31db5c4ab850ddf08028b94df523a66ca00a06082a8648ce3d030107a144034200044f5f301c270532c412f62e1333927337bd0c60e071902e0623e0532725bca5d3a281a4c80c019144710bea802212627120b3e45d128925c68004f68a983e42b4"
-	signature     string = "2babd4181f18fa45d3eaa08d345a700ff29201c40b1544130e48a66011b53b736c9acd2a526e7db31232877fcc01ab1d85522013e56fe1dda8d32fa7db0f3474"
+import (
+	"crypto/ecdsa"
+	"os"
 )
 
-func Start() {
+type wallet struct {
+	privateKey *ecdsa.PrivateKey
+}
 
+var w *wallet // 이걸 소문자로 써서 자유롭게 공유하는게 아니라, 아래의 Wallet 함수로 드러나게 할 예정
+
+func hasWalletFile() bool {
+	_, err := os.Stat("nomadcoin.wallet") // 파일이 존재하는지
+	return !os.IsNotExist(err)            // os.Stat에서 받아온 err를 확인하고 지갑 파일이 없다면 true
+}
+
+func Wallet() *wallet {
+	if w == nil {
+		// 지갑의 유뮤 확인
+		if hasWalletFile() {
+			// 없다면 비공개키를 생성해서 파일에 저장
+		} else {
+			// 이미 있다면 파일로부터 지갑을 복구
+		}
+	}
+	return w
 }
