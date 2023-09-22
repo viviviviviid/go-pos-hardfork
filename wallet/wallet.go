b2255a76b6e51152b43bb10ade2e4c3870382d8c
taptap-kor <tyy8282@gmail.com>
2023-09-23 03:22:41 +0900

    feat: restoring wallet

--- wallet/wallet.go
+++ wallet/wallet.go
@@ -16,6 +16,7 @@ const (
 
 type wallet struct {
 	privateKey *ecdsa.PrivateKey
+	address    string
 }
 
 var w *wallet // 이걸 소문자로 써서 자유롭게 공유하는게 아니라, 아래의 Wallet 함수로 드러나게 할 예정
@@ -37,6 +38,18 @@ func persistKey(key *ecdsa.PrivateKey) { // key 저장
 	err = os.WriteFile(fileName, bytes, 0644) // 0644 : 읽기와 쓰기 허용
 }
 
+func restoreKey() (key *ecdsa.PrivateKey) { // *ecdsa.PrivateKey 형식의 key를 선언 및 초기화
+	keyAsBytes, err := os.ReadFile(fileName)
+	utils.HandleErr(err)
+	key, err = x509.ParseECPrivateKey(keyAsBytes) // 이미 함수의 반환 구조에서 초기화되었으므로 key를 갱신만 해줘도 됨.
+	utils.HandleErr(err)
+	return // 함수의 반환 구조에서 뭘 반환할지 알려줬으므로, return 다음에 뭔가를 안써줘도 됨
+} // return에 비어있는지 아닌지 확인해야하므로 긴 함수에서는 귀찮음이 가중될 수 있음 -> 알고만 있기
+
+func aFromK(key *ecdsa.PrivateKey) string {
+
+}
+
 func Wallet() *wallet {
 	if w == nil {
 		w = &wallet{}
@@ -44,6 +57,7 @@ func Wallet() *wallet {
 		// 지갑의 유뮤 확인
 		if hasWalletFile() {
 			// 이미 있다면 파일로부터 지갑을 복구
+			w.privateKey = restoreKey()
 
 		} else {
 			// 없다면 비공개키를 생성해서 파일에 저장
@@ -51,6 +65,7 @@ func Wallet() *wallet {
 			persistKey(key)
 			w.privateKey = key
 		}
+		w.address = aFromK(w.privateKey)
 
 	}
 	return w
