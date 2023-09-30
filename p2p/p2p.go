f780d62e62934ccbeebf9a17c08dff176c574721
taptap-kor <tyy8282@gmail.com>
2023-09-30 20:06:42 +0900

    feat: ReadMessage in WebSocket

--- p2p/p2p.go
+++ p2p/p2p.go
@@ -1,6 +1,7 @@
 package p2p
 
 import (
+	"fmt"
 	"net/http"
 
 	"github.com/gorilla/websocket"
@@ -10,6 +11,14 @@ import (
 var upgrader = websocket.Upgrader{}
 
 func Upgrade(rw http.ResponseWriter, r *http.Request) {
-	_, err := upgrader.Upgrade(rw, r, nil)
+	upgrader.CheckOrigin = func(r *http.Request) bool { // 웹소켓 연결 허가
+		return true
+	}
+	conn, err := upgrader.Upgrade(rw, r, nil)
 	utils.HandleErr(err)
+	for {
+		_, payload, err := conn.ReadMessage()
+		utils.HandleErr(err)
+		fmt.Printf("%s\n\n", payload)
+	}
 }
