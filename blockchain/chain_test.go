6b757f54ab7ebd45a603ede02127f8e3e5234da4
taptap-kor <tyy8282@gmail.com>
2023-10-19 23:56:16 +0900

    build

--- blockchain/chain_test.go
+++ blockchain/chain_test.go
@@ -96,6 +96,7 @@ func TestFindTx(t *testing.T) {
 			t.Error("Tx should be not found.")
 		}
 	})
+
 	t.Run("Tx should be found", func(t *testing.T) {
 		dbStorage = fakeDB{
 			fakeFindBlock: func() []byte {
