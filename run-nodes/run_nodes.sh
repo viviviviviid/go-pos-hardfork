a6aba9a9962cd0e7eb73c99f83e32701b3aaee3b
taptap-kor <tyy8282@gmail.com>
2023-10-23 17:43:51 +0900

    feat: nodes' openPort input to staking transaction's value of DataInput

--- run-nodes/run_nodes.sh
+++ run-nodes/run_nodes.sh
@@ -3,6 +3,7 @@
 # logs 폴더가 존재하지 않으면 생성
 mkdir -p logs
 
+go run ../main.go -mode=rest -port=3000 > logs/log_3000.log 2>&1 &
 for ((i=0; i<10; i++)); do
     port=$((4000 + $i))
     go run ../main.go -mode=rest -port=$port > logs/log_$port.log 2>&1 &
